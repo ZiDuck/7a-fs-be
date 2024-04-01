a730a4b5c9b0a30335fcb496947b47a39f50fa83
Đăng Trần <tphdang.dev@gmail.com>
2024-04-01 17:02:35 +0700

    add event-emitter lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,6 @@ import { UserSessionsModule } from './modules/user-sessions/user-sessions.module
 import { EmailModule } from './modules/email/email.module';
 import { ClsModule } from 'nestjs-cls';
 import { CloudinaryModule } from './modules/cloudinary/cloudinary.module';
-import { CloudinaryService } from './modules/cloudinary/cloudinary.service';
 import { UploadFileModule } from './modules/upload-file/upload-file.module';
 import { ImagesModule } from './modules/images/images.module';
 import { ValidatorModule } from './common/validator/validator.module';
@@ -17,6 +16,8 @@ import { FormsModule } from './modules/forms/forms.module';
 import { FormQuestionsModule } from './modules/form-questions/form-questions.module';
 import { SingleQuestionsModule } from './modules/single-questions/single-questions.module';
 import { GroupQuestionsModule } from './modules/group-questions/group-questions.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
     imports: [
@@ -42,6 +43,8 @@ import { GroupQuestionsModule } from './modules/group-questions/group-questions.
         FormQuestionsModule,
         SingleQuestionsModule,
         GroupQuestionsModule,
+        NotificationsModule,
+        EventEmitterModule.forRoot(),
     ],
     controllers: [AppController],
     providers: [AppService],
