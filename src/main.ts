bca22e98feff6b7b0cc2fb477c224b8f10a4042f
ZiDuck <vophamduyduc.dev@gmail.com>
2024-05-22 14:10:24 +0700

    feat: Add global API prefix

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule);
 
+    app.setGlobalPrefix('api');
+
     app.enableCors({
         origin: '*',
         methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
