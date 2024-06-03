d56b11e5eb8b7e9269ad7e9e3859e2bd8fe80f26
ZiDuck <vophamduyduc.dev@gmail.com>
2024-06-03 16:40:05 +0700

    bug: fix

--- src/modules/form-summary/dto/answer-summary.dto.ts
+++ src/modules/form-summary/dto/answer-summary.dto.ts
@@ -28,10 +28,10 @@ export class AnswerSummary {
         this.answerResponses = data.answerResponses!.map((a) => {
             if ('value' in a) {
                 return new AnswerResponseCheckbox(a);
-            } else if ('filename' in a) {
+            } else if ('secureUrl' in a) {
                 return new AnswerResponseFileUpload(a);
             } else {
-                // return new RowResponse(a);
+                return new RowResponse(a);
             }
         });
     }
